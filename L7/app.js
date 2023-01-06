4337311a93e066dd6f66463c374b643dce7e5caf
Vybhavi Bukka <108615671+Vybhavi14@users.noreply.github.com>
2023-01-06 17:57:51 +0530

    Update app.js

--- L7/app.js
+++ L7/app.js
@@ -4,12 +4,9 @@ const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { response } = require("express");
-
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
-// app.use(express.static("public"));
-
 app.get("/", async (req, res) => {
   const allTodos = await Todo.getTodos();
   if (req.accepts("html")) {
@@ -20,7 +17,6 @@ app.get("/", async (req, res) => {
     res.json(allTodos);
   }
 });
-
 app.get("/todos", async (req, res) => {
   try {
     const todos = await Todo.findAll({ order: [["id", "ASC"]] });
@@ -30,7 +26,6 @@ app.get("/todos", async (req, res) => {
     return res.status(422).json(error);
   }
 });
-
 app.post("/todos", async (req, res) => {
   console.log("Body : ", req.body);
   try {
@@ -45,7 +40,6 @@ app.post("/todos", async (req, res) => {
     return response.status(422).json(error);
   }
 });
-
 app.put("/todos/:id/markAsCompleted", async (req, res) => {
   console.log("Todo marks completed : ", req.params.id);
   const todo = await Todo.findByPk(req.params.id);
@@ -57,12 +51,9 @@ app.put("/todos/:id/markAsCompleted", async (req, res) => {
     return response.status(422).json(error);
   }
 });
-
-// eslint-disable-next-line no-unused-vars
 app.delete("/todos/:id", async (req, res) => {
   console.log("We have to delete a Todo with ID: ", req.params.id);
   const affectedRow = await Todo.destroy({ where: { id: req.params.id } });
   res.send(affectedRow ? true : false);
 });
-
 module.exports = app;
